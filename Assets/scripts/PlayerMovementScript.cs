feb979593e859fbf1695b47e8e07af2b974cf374
Rigved Koli <rigvedkoli24@gmail.com>
2023-07-03 13:53:11 +0530

    Flipping enabled

--- Assets/scripts/PlayerMovementScript.cs
+++ Assets/scripts/PlayerMovementScript.cs
@@ -19,6 +19,7 @@ public class PlayerMovementScript : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     } 
 
     void OnMove(InputValue value)
@@ -32,5 +33,13 @@ public class PlayerMovementScript : MonoBehaviour
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y); 
         myRigidbody.velocity = playerVelocity;   
     }
-
+    
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x)*0.2f, 0.2f);
+        }
+    }
 }
